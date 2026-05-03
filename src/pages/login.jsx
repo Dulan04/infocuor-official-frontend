48f5a041df1abb21447c8cc1700e8553aa8fcd04
Dulan04 <dulanmadushika04@gmail.com>
2026-05-03 20:00:14 +0530

    Update Merged error 😉

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import toast from 'react-hot-toast';
+import toast, { Toaster } from 'react-hot-toast';
 import { useAuth } from "../context/AuthContext";
 
 export default function LoginPage() {
